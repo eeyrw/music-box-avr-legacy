1e8426ef5f4140ed8a962e27e9fb74872b0105c7
yuan <yrw@sdec.com.cn>
2019-11-20 15:08:24 +0800

    M

--- main.c
+++ main.c
@@ -36,6 +36,21 @@ void USART0_Init(uint32_t baud)
 	UCSR0C = (1<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);
 }
 
+void TIMER_Init()
+{
+	PORTB=0b001101;		//Initalize Port B
+	DDRB=0b000111;		
+
+	TCCR1A=0b10100001;
+	//ICNC1 ICES1 – WGM13 WGM12 CS12 CS11 CS10 ; Fast PWM pclk/1
+	TCCR1B=0b00001001;
+
+	OCR0A=62;		//Initalize TC0 in 32 kHz interval timer ( pclk=16M )
+	TCCR0A=0b00000010;
+	TCCR0B=0b00000010; //pclk/8
+	//TIMSK0=1<<OCIE0A; //Enable timer interrupt
+}
+
 ISR(USART_RX_vect)
 {
 	unsigned char data;
@@ -43,10 +58,18 @@ ISR(USART_RX_vect)
 	/* Calculate buffer index */
 
 }
+
+ISR(TIMER0_COMPA_vect)
+{
+	
+
+}
+
 int main(void)
 {
 	CLKPR=0b10000000;
 	USART0_Init(115200);
+	TIMER_Init();
 	stdout = &uart0Stdout;
 	printf("UART works!\n");
 	sei();
