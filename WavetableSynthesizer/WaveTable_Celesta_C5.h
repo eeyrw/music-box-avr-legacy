82d2d22df8bc321bd57335ec81826d673146933d
yuan <yrw@sdec.com.cn>
2019-11-21 13:58:01 +0800

    M

--- WavetableSynthesizer/WaveTable_Celesta_C5.h
+++ WavetableSynthesizer/WaveTable_Celesta_C5.h
@@ -1,6 +1,8 @@
 #ifndef __WAVETABLE_CELESTA_C5__
 #define __WAVETABLE_CELESTA_C5__
 #include <stdint.h>
+// Sample's base frequency: 523.629906 Hz
+// Sample's sample rate: 32000 Hz
 #define WAVETABLE_CELESTA_C5_LEN 2608
 #define WAVETABLE_CELESTA_C5_ATTACK_LEN 1998
 #define WAVETABLE_CELESTA_C5_LOOP_LEN 610
